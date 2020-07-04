315eef7f94931ce8fba9a23ad4cd6310e38fd123
David Zhang <straivers98@gmail.com>
2020-07-04 14:31:58 -0400

    Implement HashTable and KeyTable
    KeyTable is intended for use for the string table, and maybe some other things

--- arc/source/arc/data/hash.d
+++ arc/source/arc/data/hash.d
@@ -12,3 +12,13 @@ Hash digest(const(char)[] text) pure {
 
     return (cast(ulong*) key.ptr)[0];
 }
+
+Hash hash_of(T)(auto ref T t) {
+    import std.digest.murmurhash : hash = digest, MurmurHash3;
+
+    static if (__traits(compiles, t.toHash()))
+        return t.toHash();
+    
+    auto key = hash!(MurmurHash3!128)((&t)[0 .. 1]);
+    return (cast(ulong*) key.ptr)[0];
+}
